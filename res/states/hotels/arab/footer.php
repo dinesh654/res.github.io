f406e8228278ba4c0aa138075a62fe0410e8a0a0
dinesh654 <44696382+dinesh654@users.noreply.github.com>
2018-11-02 16:40:48 +0530

    first commit

---
+++ res/states/hotels/arab/footer.php
@@ -0,0 +1,69 @@
+<div class="clearfix"></div>
+<section id="contact">
+    <div class="container">
+        <div class="row">
+          <div class="foot_logo">
+            <div class="col-md-3">
+                <img src="images/arab.jpg" class="img-responsive" alt=""  style="margin-top:4%;" width="100" height="100"/>
+              <div class="clearfix" style="margin:20px auto;"></div>
+            	<img src="images/footer_bot.png" class="img-responsive" alt="" /> 
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="footer_about">
+			<font color="Green">
+                <h4>About Us</h4>
+                <p>Indians are known for their hospitality and every person in our team here at Sapthagiri follows the age-old tradition. We are all it takes to make you feel welcome and adeptly served. 
+
+We invite you to experience exquisite dining that will have you visiting us again and again.</p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="footer_open">
+			<font color="Green">
+                <h4>Opening Hours</h4>
+                <p style="line-height:13px;">Mon ........ 11AM - 11:30PM</p>
+                <p style="line-height:13px;">Tue ......... 10AM - 11:30PM</p>
+                <p style="line-height:13px;">Wed ........ 10AM - 11:30PM</p>
+                <p style="line-height:13px;">Thu ......... 10AM - 11:30PM</p>
+                <p style="line-height:13px;">Fri ........... 10AM - 11:30PM</p>
+                <p style="line-height:13px;">Sat .......... 9AM - 11:59PM</p>
+                <p style="line-height:13px;">Sun ......... 9AM - 11:59PM</p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="footer_contact">
+			<font color="Green">
+                <h4>Locate Us</h4>
+                <p>Arab restaurant<p>
+                    <p>tirupathi highway,chittoor <br />Andhra Pradesh, India </p>
+                       <p>Mobile: +91 9505561569</p>
+                       <p style="color:#0CF;">Email: dineshvit8@gmail.com</p> 
+                        
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div class="sub-footer">
+        <div class="container">
+         <div class="row">
+          <div class="col-xs-12">
+           <div class="col-sm-2">
+            <p class="sub-text">
+                <a href="https://www.facebook.com/freeonlineprojects/" target="_blank"><i class="fa fa-facebook"></i></a>       
+                <a href="https://twitter.com/freetimelearn" target="_blank"><i class="fa fa-twitter"></i></a>
+                <a href="https://plus.google.com/101612697119159092378" target="_blank"><i class="fa fa-google-plus"></i></a>
+            </p>    
+        </div>
+      
+        <div class="col-sm-1"><p class="sub-text link"><a href="booking_menu.php">ORDER</a></p></div>
+        <div class="col-sm-1"><p class="sub-text link"><a href="contact-us.php">Contact</a></p></div>
+        <div class="col-sm-1"><p class="sub-text link"><a href="admin.php">Admin</a></p></div>
+        <div class="col-sm-6" style="padding-top:10px;" align="center"><span style="color:#FFF;">All Copy &copy; <?php echo date('Y'); ?> rights &amp; Designed by Dinesh &nbsp;Amit &nbsp; sanith &nbsp; singh &nbsp; praneeth &nbsp;</span></div>
+    </div>
+</div>
+</div>
+</div>      
+</div>
+</div>
+</section> 
---
+++ res/states/hotels/sapthagiri/about-us.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Food ordering system</title>
+     <?php 
+  	   include('css_files.php'); 
+  	 ?>  
+ </head>
+ 
+ 
+ <body>
+
+     <div class="loader"><img src="images/loading.gif" alt="" /></div>
+        <!--banner section-->
+        <section>
+         <div class="banner">
+         <img class="img-responsive logo1" src="images/logos.jpg"/ width="200" height="100">
+		<center><img src="images/food.jpeg"  class="img-responsive" / width="500" height="100"></center>
+	</div>
+      <!--banner section close-->  
+      <?php 
+  	   include('nav_bar.php'); 
+  	 ?>
+</section>
+<div class="clearfix"></div> 
+<!--About -->
+<section>
+    <div class="container">
+        <div class="row">
+            <!--about text-->
+            <div class="about">
+                <div class="section-title2">
+                     <div class="main_h3" style="color:#0CF;" align="center">The History</div>
+                     <div class="line" align="center"><img class="img-responsive" src="images/undr.png" /></div> 
+                 </div>
+                             <div class="inner2">
+                                <p>Sapthagiri restaurant</p>
+                                 <span></span>
+                                 </div>
+                                 <div class="abt-cntnt">
+                <p> Sapthagiri is the soul of southern India and we stand for authentic Indian cuisine. Here at Sapthagiri, our aim is to bring to your table the essence of our rich heritage of taste and culinary culture. Our chefs excel in treating you with the most delectable spread and we have proved ourselves over the years of being the ones to present the same to you in the most outstanding manner. Our restaurant has a milieu that is comforting and enjoyable with spacious and sophisticated interiors and service crews that will make you feel special, welcome and relaxed. .</p>
+            </div>
+            </div>
+            </div>
+            <!--about text close-->
+            </div>
+            
+            </section>
+ 
+    <div class="container">
+            <div class="section-title2"></div>
+            <div class="row">
+               <div class="col-md-4" align="center"><img src="images/thumb1.jpg" class="img img-circle" width="270" height="220" alt="" /></div>
+            <div class="col-md-8">
+              <div class="main_h3" style="color:#0CF;">What We Can Do</div>
+              <ul class="list icons"><font color="orange">
+                <li><i class="fa fa-cutlery"></i>we can acomodate 40 people</li>
+                <li><i class="fa fa-leaf"></i>fresh food every day, produced locally from our neightbours</li>
+                <li><i class="fa fa-trophy"></i>world renouned chefs with 20 years experience</li>
+                <li><i class="fa fa-glass"></i>need a romantic getaway, kateleya is for you</li>
+                <li><i class="fa fa-coffee"></i>or maybe a business dinner, try us now or just come for a cupcontact us of exelent coffee</li></font>
+              </ul>
+          </div>
+       </div>
+   </div>           
+</section>
+
+
+<!-- Start Our Specials -->
+<section>
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="">
+<div class="section-title2">
+      <div class="main_h3" style="color:#0CF;" align="center">Our Specials</div>
+         <div class="line" align="center"><img class="img-responsive" src="images/undr.png" /></div> 
+    </div>
+     <div class="inner2">
+        <p>These fine folks trusted tha award winning restaurant</p>
+         <span></span>
+         </div>
+          <div class="marq">
+    <marquee direction="left" scrollamount="10" onmouseover="this.stop();" onmouseout="this.start();">
+    <img src="images/1-1.jpg" style="padding-right:15px;" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <img src="images/1-2.jpg" style="padding-right:15px;" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <img src="images/1-3.jpg" style="padding-right:15px;" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <img src="images/1-4.jpg" style="padding-right:15px;" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <img src="images/1-5.jpg" style="padding-right:15px;" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <img src="images/1-6.jpg" style="padding-right:15px;" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </marquee>
+    </div>
+   </div>
+  </div>
+ </div>
+</div>
+</section> 
+<!-- End Our Specials -->        
+
+<?php 
+	include('footer.php');
+	include('js_files.php');
+?>
+</body>
+</html>
